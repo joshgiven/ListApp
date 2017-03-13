4b356e2b087be8009836921dd3a3e8c86f88928e
Geoff <geoff@Geoffs-MacBook-Pro.local>
2017-03-13 16:32:55 -0600

    Fixed Tstatus name in persistancexml

--- TrailAppJPA/build/resources/test/entities/User.java
+++ TrailAppJPA/build/resources/test/entities/User.java
@@ -1,9 +1,16 @@
 package entities;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -13,34 +20,43 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private String username;
-	private String password;
+	
 	private String email;
 	
-	public User() {}
-
-	public User(int id, String username, String password, String email) {
+	private String password;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="description")
+	private String description;
+	
+	@ManyToMany
+	@JoinTable(name="user_trail", 
+	joinColumns=@JoinColumn(name="user_id", referencedColumnName="id"),
+	inverseJoinColumns=@JoinColumn(name="trail_id", referencedColumnName="id"))
+	private List<Trail> favorites;
+	
+	@OneToMany(mappedBy="user")
+	private List<Report> reports;
+	
+	
+	public User(){}
+	
+	public User(int id, String email, String password, String firstName, String lastName, String description,
+			List<Trail> favorites, List<Report> reports) {
 		super();
 		this.id = id;
-		this.username = username;
-		this.password = password;
 		this.email = email;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
 		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.description = description;
+		this.favorites = favorites;
+		this.reports = reports;
 	}
 
 	public String getEmail() {
@@ -51,48 +67,57 @@ public class User {
 		this.email = email;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Trail> getFavorites() {
+		return favorites;
+	}
+
+	public void setFavorites(List<Trail> favorites) {
+		this.favorites = favorites;
+	}
+
+	public List<Report> getReports() {
+		return reports;
+	}
+
+	public void setReports(List<Report> reports) {
+		this.reports = reports;
+	}
+
 	public int getId() {
 		return id;
 	}
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", email=" + email + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + id;
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (id != other.id)
-			return false;
-		if (username == null) {
-			if (other.username != null)
-				return false;
-		} else if (!username.equals(other.username))
-			return false;
-		return true;
-	}
-	
 	
 }
