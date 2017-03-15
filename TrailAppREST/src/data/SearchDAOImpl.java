af2bbed94badafbf4ba8ca7084f4c05439bb8040
chazlegrand <chaz.sroczynski@gmail.com>
2017-03-15 13:56:27 -0600

    modified search by radius

--- TrailAppREST/src/data/SearchDAOImpl.java
+++ TrailAppREST/src/data/SearchDAOImpl.java
@@ -75,13 +75,29 @@ public class SearchDAOImpl implements SearchDAO {
 				.setParameter("maxLat", maxLat).setParameter("minLong", minLong).setParameter("maxLong", maxLong)
 				.getResultList();
 		
-	
-
+		double maxDistInKM = rangeInMiles * 1.61;
+		
+		for (Trail trail : trails) {
+			double dist = haversine(trail.getLatitude(), trail.getLongitude(), originLat, originLong);
+			if (dist > maxDistInKM){
+				trails.remove(trail);
+			}
+		}
 		
 		return trails;
 	}
 	
-	
+	public static double haversine(double lat1, double lng1, double lat2,  double lng2) {
+	    int r = 6371; // average radius of the earth in km
+	    double dLat = Math.toRadians(lat2 - lat1);
+	    double dLon = Math.toRadians(lng2 - lng1);
+	    double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+	       Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) 
+	      * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    double d = r * c;
+	    return d;
+	}
 	
 	
 
