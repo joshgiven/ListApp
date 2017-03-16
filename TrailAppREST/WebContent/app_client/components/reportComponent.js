8b13bc256738e2191fcaf8c6c4d71463aec4d728
Josh Given <josh.given@gmail.com>
2017-03-16 13:29:06 -0600

    reports now contain author

--- TrailAppREST/WebContent/app_client/components/reportComponent.js
+++ TrailAppREST/WebContent/app_client/components/reportComponent.js
@@ -4,18 +4,41 @@ var reportController = function() {
   var ctrl = this;
 };
 
+
+var tmp =
+  {
+    "tstatuses" : [
+      {
+        "id":5,
+        "name":"no snow",
+        "statusType":"snow"
+      },
+      {
+        "id":8,
+        "name":"washed out",
+        "statusType":"ground"
+      },
+      {
+        "id":15,
+        "name":"hazardous",
+        "statusType":"passability"
+      }]
+  };
+
 module.component('reportComponent', {
   template : `
       <div class="">
         <h4>{{$ctrl.report.heading}}</h4>
-        <p>{{$ctrl.report.comment}}</p>
-        <p>{{$ctrl.report.timestamp}}</p>
-        <p>{{$ctrl.report.user.firstName}}</p>
-        <p>{{$ctrl.report.user.lastName}}</p>
-        <!-- $ctrl.report.trail -->
-        <div ng-repeat="status in $ctrl.report.tstatuses">
-          <p>{{tstatus.statusType }}</p>
-          <p>{{tstatus.name}}</p>
+        <p>comment: {{$ctrl.report.comment}}</p>
+        <p>time: {{$ctrl.report.timestamp}}</p>
+        <p>author: {{$ctrl.report.user.firstName}} {{$ctrl.report.user.lastName}}</p>
+        <div class="">
+          <p>status:<p>
+          <ul>
+            <li ng-repeat="status in $ctrl.report.tstatuses">
+              {{status.statusType }} : {{status.name}}
+            </li>
+          </ul>
         </div>
       </div>
     `,
--- TrailAppREST/src/controllers/AuthController.java
+++ TrailAppREST/src/controllers/AuthController.java
@@ -20,87 +20,96 @@ import security.JsonWebTokenGenerator;
 
 @RestController
 public class AuthController {
-	
-	 @Autowired
-	 JsonWebTokenGenerator jwtGen;
+
+	@Autowired
+	JsonWebTokenGenerator jwtGen;
 
 	@Autowired
 	AuthDAO authDAO;
-	
+
 	@GetMapping("auth/ping")
 	String ping() {
 		return "pong";
 	}
-	
 
-	  @PostMapping(value = "/login")
-	  public Map<String,String> login(HttpServletRequest req, HttpServletResponse res, @RequestBody String userJsonString) {
-	    System.out.println("in login method");
-		  System.out.println(userJsonString);
-		 ObjectMapper mapper = new ObjectMapper();
-	    User user = null;
-	    // Parse User from JSON
-	    try {
-	      user = mapper.readValue(userJsonString, User.class);
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    }
-	    // Find managed User, return it if password is correct
-	    try {
-	      user = authDAO.authenticateUser(user);
-	    } catch (Exception e) {
-	      // User not authenticated
-	      e.printStackTrace();
-	      res.setStatus(401);
-	      return null;
-	    }
-	    
-	    String jws = jwtGen.generateUserJwt(user);
-	    Map<String, String> responseJson = new HashMap<>();
-	    responseJson.put("jwt", jws);
-	    System.out.println(jws);
-	    return responseJson;
-	  }
-	  
-	  @PostMapping(value = "/signup")
-	  public Map<String,String> register(HttpServletRequest req, HttpServletResponse res, @RequestBody String userJsonString) {
-		  System.out.println("in signup method");
-		  System.out.println(userJsonString);
-		  ObjectMapper mapper = new ObjectMapper();
-		  User user = null;
-		  // Parse User from JSON
-		  try {
-			  user = mapper.readValue(userJsonString, User.class);
-		  } catch (Exception e) {
-			  e.printStackTrace();
-		  }
-		  // Find managed User, return it if password is correct
-		  try {
-			  user = authDAO.create(user);
-		  } catch (Exception e) {
-			  // User not authenticated
-			  e.printStackTrace();
-			  res.setStatus(401);
-			  return null;
-		  }
-		  
-		  String jws = jwtGen.generateUserJwt(user);
-		  Map<String, String> responseJson = new HashMap<>();
-		  responseJson.put("jwt", jws);
-		  return responseJson;
-	  }
-	
-//	@PostMapping(path="/register")
-//		public User register(@RequestBody String userJSON){
-//		ObjectMapper mapper = new ObjectMapper();
-//		User newUser = null;
-//		try {
-//			newUser = mapper.readValue(userJSON, User.class);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		return	authDAO.register(newUser);
-//	}
+	@PostMapping(value = "/login")
+	public Map<String, String> login(HttpServletRequest req, HttpServletResponse res,
+			@RequestBody String userJsonString) {
+		
+		System.out.println("in login method");
+		System.out.println(userJsonString);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		User user = null;
+		// Parse User from JSON
+		try {
+			user = mapper.readValue(userJsonString, User.class);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(user);
+		
+		// Find managed User, return it if password is correct
+		try {
+			user = authDAO.authenticateUser(user);
+		} 
+		catch (Exception e) {
+			// User not authenticated
+			e.printStackTrace();
+			res.setStatus(401);
+			return null;
+		}
+
+		String jws = jwtGen.generateUserJwt(user);
+		Map<String, String> responseJson = new HashMap<>();
+		responseJson.put("jwt", jws);
+		System.out.println(jws);
+		return responseJson;
+	}
+
+	@PostMapping(value = "/signup")
+	public Map<String, String> register(HttpServletRequest req, HttpServletResponse res,
+			@RequestBody String userJsonString) {
+		System.out.println("in signup method");
+		System.out.println(userJsonString);
+		ObjectMapper mapper = new ObjectMapper();
+		User user = null;
+		// Parse User from JSON
+		try {
+			user = mapper.readValue(userJsonString, User.class);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		// Find managed User, return it if password is correct
+		try {
+			user = authDAO.create(user);
+		} 
+		catch (Exception e) {
+			// User not authenticated
+			e.printStackTrace();
+			res.setStatus(401);
+			return null;
+		}
+
+		String jws = jwtGen.generateUserJwt(user);
+		Map<String, String> responseJson = new HashMap<>();
+		responseJson.put("jwt", jws);
+		return responseJson;
+	}
+
+	// @PostMapping(path="/register")
+	// public User register(@RequestBody String userJSON){
+	// ObjectMapper mapper = new ObjectMapper();
+	// User newUser = null;
+	// try {
+	// newUser = mapper.readValue(userJSON, User.class);
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// return authDAO.register(newUser);
+	// }
 
 }
-
