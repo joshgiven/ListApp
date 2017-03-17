01b7da561a245cfb618aef7d4431f4a3e76b78be
Geoff <geoff@Geoffs-MacBook-Pro.local>
2017-03-17 17:31:43 -0600

    report stuff

--- TrailAppREST/WebContent/app_client/components/reportComponent.js
+++ TrailAppREST/WebContent/app_client/components/reportComponent.js
@@ -1,7 +1,17 @@
 var module = angular.module('ngTrailApp');
 
-var reportController = function() {
+var reportController = function(authService) {
   var ctrl = this;
+
+  ctrl.user = authService.currentUser();
+
+  ctrl.owner = function(report){
+    if (ctrl.user.id === report.user.id) {
+      return true;
+    }
+    else return false;
+  };
+
   console.log("report quiet in reportComponent: " + ctrl.reportQuiet);
 };
 
@@ -11,6 +21,11 @@ module.component('reportComponent', {
   template : `
       <div class="">
         <h4 ng-hide="$ctrl.reportQuiet">{{$ctrl.report.heading}}</h4>
+        <button class="btn btn-primary btn-lg" ng-show=$ctrl.owner($ctrl.report)
+          ng-click=" >Edit</button>
+          <div>
+          <report-form-component report=$ctrl.report >loading....</report-form-component>
+          </div>
         <p ng-hide="$ctrl.reportQuiet">comment: {{$ctrl.report.comment}}</p>
         <p>Last Updated: {{$ctrl.report.timestamp | date : 'medium' : '-1200' }}</p>
         <p ng-hide="$ctrl.reportQuiet">author: {{$ctrl.report.user.firstName}} {{$ctrl.report.user.lastName}}</p>
