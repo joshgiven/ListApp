0a2a7cb3361b2be4b7f024ef3f8b6afb794a1480
chazlegrand <chaz.sroczynski@gmail.com>
2017-03-19 10:31:57 -0600

    Stopped fform from showing up on every search page

--- TrailAppREST/WebContent/app_client/components/reportComponent.js
+++ TrailAppREST/WebContent/app_client/components/reportComponent.js
@@ -20,7 +20,7 @@ module.component('reportComponent', {
         <button class="btn btn-primary btn-lg" ng-show=$ctrl.owner($ctrl.report)
                 ng-click="" >Edit</button>
           <div>
-          <report-form-component report=$ctrl.report >loading....</report-form-component>
+          <!-- <report-form-component report=$ctrl.report >loading....</report-form-component> -->
           </div>
         <p ng-hide="$ctrl.reportQuiet">comment: {{$ctrl.report.comment}}</p>
         <p>Last Updated: {{$ctrl.report.timestamp | date : 'medium' : '-1200' }}</p>
